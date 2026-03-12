04d3c4e4e628b7b2f83475d26e1002c3b114f20a
Pavlo Asieiev <726030@student.inholland.nl>
2026-03-12 15:31:16 +0100

    get the text from url

--- main.py
+++ main.py
@@ -3,3 +3,17 @@ import urllib.request
 import zipfile
 import numpy as np
 from collections import Counter
+
+def get_text_data():
+    """Downloads and reads a chunk of the text8 dataset."""
+    try:
+        urllib.request.urlretrieve("http://mattmahoney.net/dc/text8.zip", "text8.zip")
+    except Exception as e:
+        print(f"Error downloading: {e}.")
+        return ""
+    
+    with zipfile.ZipFile("text8.zip") as f:
+        text = f.read("text8").decode("utf-8")
+    
+    # Using only first 5 million characters
+    return text[:5_000_000] 
